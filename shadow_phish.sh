b59d25d3985e85446e9eeabb13a94c7e772192ae
SHADOW-SQUAD-BROTHEr <78258812+SHADOW-SQUAD-BROTHEr@users.noreply.github.com>
2021-01-31 14:51:51 +0600

    Update shadow_phish.sh

--- shadow_phish.sh
+++ shadow_phish.sh
@@ -9,7 +9,6 @@ MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\0
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
 RESETBG="$(printf '\e[0m\n')"
-
 ## Directories
 if [[ ! -d ".server" ]]; then
 	mkdir -p ".server"
@@ -21,8 +20,7 @@ else
 	mkdir -p ".server/www"
 fi
 
-## Script termination
-exit_on_signal_SIGINT() 
+## Script termination 
     { printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
     exit 0
 }
